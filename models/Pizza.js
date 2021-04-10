dce6005fe6d8633acfc3ca031303ef5771d171cb
Fire Dragon <daniwhitlock4@gmail.com>
2021-04-10 09:20:44 -0600

    finished code

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,13 +1,16 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-
 const PizzaSchema = new Schema({
     pizzaName: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     createdBy: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     createdAt: {
         type: Date,
@@ -16,6 +19,8 @@ const PizzaSchema = new Schema({
     },
     size: {
         type: String,
+        required: true,
+        enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
         default: 'Large'
     },
     toppings: [], // You could also specify Array instead of brackets
@@ -31,14 +36,19 @@ const PizzaSchema = new Schema({
             virtuals: true,
             getters: true
         },
+         // prevents virtuals from creating duplicate of _id as `id`
         id: false
     }
 );
 
+
 // get total count of comments and replies on retrieval
-PizzaSchema.virtual('commentCount').get(function () {
-    return this.comments.length;
-});
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.reduce(
+      (total, comment) => total + comment.replies.length + 1,
+      0
+    );
+  });
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
